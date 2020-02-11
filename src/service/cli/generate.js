51dace582341c9fade93f224b5c34347b83ed40e
iashchuk <221box@list.ru>
2020-02-11 22:31:37 +0300

    Реализует запись моков через async/await

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
 const {getRandomInt, shuffle} = require(`../../utils`);
@@ -84,7 +84,7 @@ const generateOffers = (count) =>
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
 
     if (count !== undefined && isNaN(count)) {
@@ -101,14 +101,13 @@ module.exports = {
 
     const content = JSON.stringify(generateOffers(countOffer));
 
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        console.error(chalk.red(`Невозможно записать данные в файл.`));
-        process.exit(ExitCode.error);
-      }
-
+    try {
+      await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`Операция выполнена успешно. Файл создан.`));
       process.exit(ExitCode.success);
-    });
+    } catch (err) {
+      console.error(chalk.red(`Невозможно записать данные в файл.`));
+      process.exit(ExitCode.error);
+    }
   }
 };
