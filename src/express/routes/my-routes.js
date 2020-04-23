701d9761eb0fa230626de31ab14a7fe155afa17e
iashchuk <221box@list.ru>
2020-04-24 01:38:58 +0300

    Делает рефакторинг шаблонов для my_routes

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,8 +2,10 @@
 
 const {Router} = require(`express`);
 const myRouter = new Router();
+const {notes, myComments} = require(`../mocks`);
 
-myRouter.get(`/`, (req, res) => res.render(`my/my`));
-myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
+
+myRouter.get(`/`, (req, res) => res.render(`my/my`, {notes}));
+myRouter.get(`/comments`, (req, res) => res.render(`my/comments`, {comments: myComments}));
 
 module.exports = myRouter;
