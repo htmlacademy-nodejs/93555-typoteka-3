63a891744ebf39e4ec1d3540e9bb80fffc7e8bbf
iashchuk <iashchuk@outlook.com>
2021-10-17 02:03:14 +0300

    Реализует наполнение БД через cli

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const help = require(`./help`);
-const generate = require(`./generate`);
+const fillDb = require(`./fillDb`);
 const version = require(`./version`);
 const server = require(`./server`);
 
 const Cli = {
-  [generate.name]: generate,
+  [fillDb.name]: fillDb,
   [help.name]: help,
   [version.name]: version,
   [server.name]: server
